8582f1a6440910663611e9c32ea0d46e974b4739
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-17 18:23:15 -0300

    :sparkles: feat: iniciar aplicação utilizando o Spring

---
+++ src/main/java/com/adatech/desafiopratico/DesafiopraticoApplication.java
@@ -0,0 +1,13 @@
+package com.adatech.desafiopratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopraticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adatech/desafiopratico/DesafiopraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adatech.desafiopratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
