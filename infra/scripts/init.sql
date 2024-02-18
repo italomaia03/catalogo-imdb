d4dbd8f885f9a83a52f31e2972ef9568c8984ffd
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 14:31:46 -0300

    :wrench: chore: modificar DDL para acomodar requisitos

--- infra/scripts/init.sql
+++ infra/scripts/init.sql
@@ -1,11 +1,22 @@
 \c catalogo_filmes
+
+CREATE TABLE IF NOT EXISTS diretores
+(
+    id_diretor   SERIAL PRIMARY KEY,
+    nome_diretor VARCHAR NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS filmes
 (
     id_filme             SERIAL PRIMARY KEY,
     nome_filme           VARCHAR NOT NULL,
     duracao_total_min    INT     NOT NULL,
     avaliacao_filme      NUMERIC(2, 1),
-    ano_lancamento_filme INT     NOT NULL
+    ano_lancamento_filme INT     NOT NULL,
+    orcamento_filme BIGINT NOT NULL,
+    descricao TEXT,
+    diretor_filme INT NOT NULL,
+    FOREIGN KEY (diretor_filme) REFERENCES diretores(id_diretor)
 );
 
 CREATE TABLE IF NOT EXISTS atores
@@ -14,11 +25,6 @@ CREATE TABLE IF NOT EXISTS atores
     nome_ator VARCHAR NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS diretores
-(
-    id_diretor   SERIAL PRIMARY KEY,
-    nome_diretor VARCHAR NOT NULL UNIQUE
-);
 
 CREATE TABLE IF NOT EXISTS filme_ator
 (
@@ -27,15 +33,6 @@ CREATE TABLE IF NOT EXISTS filme_ator
     id_ator       INT NOT NULL,
     FOREIGN KEY (id_filme) REFERENCES filmes (id_filme),
     FOREIGN KEY (id_ator) REFERENCES atores (id_ator)
-);
-
-CREATE TABLE IF NOT EXISTS filme_diretor
-(
-    id_filme_diretor SERIAL PRIMARY KEY,
-    id_filme         INT NOT NULL,
-    id_diretor       INT NOT NULL,
-    FOREIGN KEY (id_filme) REFERENCES filmes (id_filme),
-    FOREIGN KEY (id_diretor) REFERENCES diretores (id_diretor)
 );
 
 
