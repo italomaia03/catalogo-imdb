95d4476cee98b0b2185dc3e7af30f445ac6f9e22
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-19 16:17:57 -0300

    :sparkles: feat: implementar serviço de cadastro de filmes

--- src/main/java/com/adatech/desafiopratico/services/FilmeService.java
+++ src/main/java/com/adatech/desafiopratico/services/FilmeService.java
@@ -1,19 +1,30 @@
 package com.adatech.desafiopratico.services;
 
+import com.adatech.desafiopratico.dto.FilmeAtorDto;
+import com.adatech.desafiopratico.dto.FilmeDto;
 import com.adatech.desafiopratico.excecoes.NaoEncontradoException;
+import com.adatech.desafiopratico.models.Ator;
+import com.adatech.desafiopratico.models.Diretor;
 import com.adatech.desafiopratico.models.Filme;
 import com.adatech.desafiopratico.repository.FilmeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class FilmeService {
 
     private FilmeRepository filmeRepository;
+    private AtorService atorService;
+    private DiretorService diretorService;
 
-    public FilmeService(FilmeRepository filmeRepository) {
+    public FilmeService(FilmeRepository filmeRepository, AtorService atorService, DiretorService diretorService) {
         this.filmeRepository = filmeRepository;
+        this.atorService = atorService;
+        this.diretorService = diretorService;
     }
 
     public List<Filme> buscarFilmes(String nomeFilme) throws NaoEncontradoException {
@@ -27,5 +38,40 @@ public class FilmeService {
             }
         }
         return filmesEncontrados;
+    }
+
+    public Filme cadastrarNovoFilme(FilmeDto filmeDto) {
+        Set<Ator> atoresFilme = new HashSet<>();
+        filmeDto.atoresFilme()
+                .stream()
+                .map(atorDto -> atorService.cadastrarNovoAtor(atorDto))
+                .forEach(atoresFilme::add);
+        Diretor diretorFilme = diretorService.cadastrarNovoDiretor(filmeDto.diretorFilme());
+        Filme novoFilme = filmeRepository.save(montarNovoFilme(filmeDto, diretorFilme, atoresFilme));
+        atoresFilme.forEach(ator -> vincularAtoresFilme(novoFilme, ator));
+        return novoFilme;
+    }
+
+    private void vincularAtoresFilme(Filme filme, Ator ator) {
+        FilmeAtorDto atorFilmeDto = new FilmeAtorDto(
+                filme.getIdFilme(), ator.getIdAtor()
+        );
+        filmeRepository.vincularAtoresFilme(atorFilmeDto);
+    }
+
+    private Filme montarNovoFilme(FilmeDto filmeDto, Diretor diretorFilme, Set<Ator> atoresSet) {
+        Filme novoFilme = new Filme();
+
+        novoFilme.setNomeFilme(filmeDto.nomeFilme());
+        novoFilme.setDuracaoTotalMinutos(filmeDto.duracaoTotalMinutos());
+        novoFilme.setAvaliacaoFilme(filmeDto.avaliacaoFilme());
+        novoFilme.setAnoLancamentoFilme(filmeDto.anoLancamentoFilme());
+        novoFilme.setOrcamentoFilme(filmeDto.orcamentoFilme());
+        novoFilme.setDescricao(filmeDto.descricao());
+        novoFilme.setDiretorFilmeId(diretorFilme.getIdDiretor());
+        novoFilme.setDiretorFilme(diretorFilme);
+        novoFilme.setAtoresFilme(atoresSet);
+
+        return novoFilme;
     }
 }
