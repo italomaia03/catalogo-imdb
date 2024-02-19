fea3a2421b54a941d5753944bfc65d5dea63e5ff
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 23:02:49 -0300

    :sparkles: feat: implementar query de cadastro de diretores

--- src/main/java/com/adatech/desafiopratico/repository/DiretorRepository.java
+++ src/main/java/com/adatech/desafiopratico/repository/DiretorRepository.java
@@ -2,8 +2,15 @@ package com.adatech.desafiopratico.repository;
 
 import com.adatech.desafiopratico.models.Diretor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface DiretorRepository extends JpaRepository<Diretor, Integer> {
+    @Transactional
+    @Query(value = "INSERT INTO diretores (nome_diretor) VALUES (:#{#diretor.getNomeDiretor()}) ON CONFLICT DO NOTHING RETURNING *", nativeQuery = true)
+    Diretor adicionarNovoDiretor(@Param("diretor") Diretor diretor);
 }
