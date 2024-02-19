f2e1bbf9d7195507638ac75ea35e97990d0aa13d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 22:54:42 -0300

    :sparkles: feat: adicionar tratamento da exceção de registros repetidos

--- src/main/java/com/adatech/desafiopratico/util/ExcecoesControllerHandler.java
+++ src/main/java/com/adatech/desafiopratico/util/ExcecoesControllerHandler.java
@@ -2,6 +2,7 @@ package com.adatech.desafiopratico.util;
 
 import com.adatech.desafiopratico.excecoes.CampoInvalidoException;
 import com.adatech.desafiopratico.excecoes.NaoEncontradoException;
+import com.adatech.desafiopratico.excecoes.RegistroRepetidoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,7 +22,14 @@ public class ExcecoesControllerHandler {
     @ExceptionHandler(NaoEncontradoException.class)
     public ResponseEntity<Object> tratarNaoEncontradoException(NaoEncontradoException naoEncontradoException){
         Map<String, String> mensagem = new HashMap<>();
-        mensagem.put("mensagem", naoEncontradoException.getMessage());
+        mensagem.put("Erro: ", naoEncontradoException.getMessage());
         return new ResponseEntity<>(mensagem, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RegistroRepetidoException.class)
+    public ResponseEntity<Object> tratarRegistroRepetidoException(RegistroRepetidoException e) {
+        Map<String, String> mensagemErro = new HashMap<>();
+        mensagemErro.put("Erro: ", e.getMessage());
+        return new ResponseEntity<>(mensagemErro, HttpStatus.CONFLICT);
     }
 }
