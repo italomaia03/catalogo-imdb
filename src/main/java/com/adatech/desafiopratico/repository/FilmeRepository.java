249cd1515eaab4736df1b76cdf61f1843c2d9100
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 22:25:10 -0300

    :sparkles: feat: criar query de busca de filmes a partir do nome

--- src/main/java/com/adatech/desafiopratico/repository/FilmeRepository.java
+++ src/main/java/com/adatech/desafiopratico/repository/FilmeRepository.java
@@ -2,8 +2,15 @@ package com.adatech.desafiopratico.repository;
 
 import com.adatech.desafiopratico.models.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface FilmeRepository extends JpaRepository<Filme, Integer> {}
+public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+    @Query(value = "SELECT * FROM filmes WHERE LOWER(nome_filme) LIKE LOWER(:nomeFilme)||'%'", nativeQuery = true)
+    Optional<List<Filme>> buscarFilmePorNome(@Param("nomeFilme") String nomeFilme);
+}
