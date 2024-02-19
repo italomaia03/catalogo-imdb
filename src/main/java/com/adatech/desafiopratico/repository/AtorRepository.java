d7620a084f1cb3f269dd0baf085cf9ac0800024d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 22:26:41 -0300

    :sparkles: feat: modificar query de cadastro de atores

--- src/main/java/com/adatech/desafiopratico/repository/AtorRepository.java
+++ src/main/java/com/adatech/desafiopratico/repository/AtorRepository.java
@@ -1,9 +1,18 @@
 package com.adatech.desafiopratico.repository;
 
+import com.adatech.desafiopratico.dto.AtorDto;
 import com.adatech.desafiopratico.models.Ator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AtorRepository extends JpaRepository<Ator, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO atores (nome_ator) VALUES (:#{#ator.getNomeAtor()}) ON CONFLICT DO NOTHING;", nativeQuery = true)
+    Integer adicionarNovoAtor(@Param("ator") Ator ator);
 }
