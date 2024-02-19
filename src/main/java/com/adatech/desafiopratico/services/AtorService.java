e960d2e4b30edcb07238ffa8fa42f3e44e09172f
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-19 16:18:33 -0300

    :sparkles: feat: modificar lógica do serviço de cadastro de atores

--- src/main/java/com/adatech/desafiopratico/services/AtorService.java
+++ src/main/java/com/adatech/desafiopratico/services/AtorService.java
@@ -1,7 +1,10 @@
 package com.adatech.desafiopratico.services;
 
 import com.adatech.desafiopratico.dto.AtorDto;
+import com.adatech.desafiopratico.dto.DiretorDto;
+import com.adatech.desafiopratico.dto.PessoaDto;
 import com.adatech.desafiopratico.models.Ator;
+import com.adatech.desafiopratico.models.Diretor;
 import com.adatech.desafiopratico.repository.AtorRepository;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +22,27 @@ public class AtorService {
         return atorRepository.findAll();
     }
 
-    public Ator cadastrarNovoAtor(AtorDto atorDto) {
-        Ator novoAtor = new Ator();
-        novoAtor.setNomeAtor(atorDto.getNome());
-        novoAtor = atorRepository.adicionarNovoAtor(novoAtor);
-        return novoAtor;
+//    public Ator cadastrarNovoAtor(AtorDto atorDto) {
+//        Ator novoAtor = new Ator();
+//        novoAtor.setNomeAtor(atorDto.getNome());
+//        novoAtor = atorRepository.adicionarNovoAtor(novoAtor);
+//        return novoAtor;
+//    }
+
+    public Ator cadastrarNovoAtor(AtorDto atorDto){
+        try {
+            return verificarAtorEstaCadastrado(atorDto);
+        } catch (NullPointerException e) {
+            Ator novoAtor = new Ator(atorDto.getNome());
+            return atorRepository.adicionarNovoAtor(novoAtor);
+        }
+    }
+
+    private Ator verificarAtorEstaCadastrado(AtorDto atorDto) {
+        Ator ator = atorRepository.findAtorByNomeAtor(atorDto.getNome());
+        if (ator == null) {
+            throw new NullPointerException();
+        }
+        return ator;
     }
 }
