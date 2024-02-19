66e25ac50d96a90a7e1fcbac064ac81a0cdd8c47
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-18 22:28:16 -0300

    :recycle: refactor: criar camada de serviços para atores

---
+++ src/main/java/com/adatech/desafiopratico/services/AtorService.java
@@ -0,0 +1,28 @@
+package com.adatech.desafiopratico.services;
+
+import com.adatech.desafiopratico.dto.AtorDto;
+import com.adatech.desafiopratico.models.Ator;
+import com.adatech.desafiopratico.repository.AtorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AtorService {
+    private AtorRepository atorRepository;
+
+    public AtorService(AtorRepository atorRepository) {
+        this.atorRepository = atorRepository;
+    }
+
+    public List<Ator> listarAtores() {
+        return atorRepository.findAll();
+    }
+
+    public Ator cadastrarNovoAtor(AtorDto atorDto) {
+        Ator novoAtor = new Ator();
+        novoAtor.setNomeAtor(atorDto.getNome());
+        System.out.println(atorRepository.adicionarNovoAtor(novoAtor));
+        return novoAtor;
+    }
+}
