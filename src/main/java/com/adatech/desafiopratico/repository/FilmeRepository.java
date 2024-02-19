a2d9a9e27e956fb2dee05c5a3530ff5188366cc3
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-19 16:17:02 -0300

    :sparkles: feat: vincular atores aos respectivos filmes

--- src/main/java/com/adatech/desafiopratico/repository/FilmeRepository.java
+++ src/main/java/com/adatech/desafiopratico/repository/FilmeRepository.java
@@ -1,10 +1,13 @@
 package com.adatech.desafiopratico.repository;
 
+import com.adatech.desafiopratico.dto.FilmeAtorDto;
 import com.adatech.desafiopratico.models.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +16,8 @@ import java.util.Optional;
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
     @Query(value = "SELECT * FROM filmes WHERE LOWER(nome_filme) LIKE LOWER(:nomeFilme)||'%'", nativeQuery = true)
     Optional<List<Filme>> buscarFilmePorNome(@Param("nomeFilme") String nomeFilme);
+    @Modifying
+    @Transactional
+    @Query (value = "INSERT INTO filme_ator (id_filme, id_ator) VALUES (:#{#filmeAtor.idFilme()}, :#{#filmeAtor.idAtor()})", nativeQuery = true)
+    void vincularAtoresFilme(@Param("filmeAtor") FilmeAtorDto filmeAtor);
 }
