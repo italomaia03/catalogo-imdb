0eab21289d181453bf7426e8bd99f649074a17b9
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-22 22:03:53 -0300

    :sparkles: feat: implementar método de validação de objetos to tipo PessoaDto

--- src/main/java/com/adatech/desafiopratico/dto/PessoaDto.java
+++ src/main/java/com/adatech/desafiopratico/dto/PessoaDto.java
@@ -1,5 +1,7 @@
 package com.adatech.desafiopratico.dto;
 
+import com.adatech.desafiopratico.dto.exceptions.DtoInvalidoException;
+
 public abstract class PessoaDto {
     private String nome;
 
@@ -21,4 +23,10 @@ public abstract class PessoaDto {
     public abstract Object mapearParaDto(Object objeto);
 
     public abstract Object mapearParaEntidade();
+
+    public void validarPessoaDto() {
+        if (this.nome == null || this.nome.trim().isEmpty()){
+            throw new DtoInvalidoException("O campo a seguir não pode ser nulo: nome");
+        }
+    }
 }
