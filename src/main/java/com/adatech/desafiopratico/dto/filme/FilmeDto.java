0876da339a47d9f1b97668c59eaca01631ffcd45
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-22 22:06:57 -0300

    :sparkles: feat: implementar validação dos atributos de filmes para novos cadastros

--- src/main/java/com/adatech/desafiopratico/dto/filme/FilmeDto.java
+++ src/main/java/com/adatech/desafiopratico/dto/filme/FilmeDto.java
@@ -2,10 +2,14 @@ package com.adatech.desafiopratico.dto.filme;
 
 import com.adatech.desafiopratico.dto.AtorDto;
 import com.adatech.desafiopratico.dto.DiretorDto;
+import com.adatech.desafiopratico.dto.exceptions.DtoInvalidoException;
 import com.adatech.desafiopratico.models.Filme;
 
+import java.lang.reflect.Field;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public record FilmeDto(
         String nomeFilme,
@@ -15,7 +19,7 @@ public record FilmeDto(
         BigInteger orcamentoFilme,
         String descricao,
         DiretorDto diretorFilme,
-        List<AtorDto> atoresFilme
+        Set<AtorDto> atoresFilme
 ) {
     public Filme mapearParaEntidade() {
         Filme novoFilme = new Filme();
@@ -31,20 +35,50 @@ public record FilmeDto(
         return novoFilme;
     }
 
-    public FilmeDto mapearParaDto(Filme filme) {
-        List<AtorDto> atoresDto = filme.getAtoresFilme().stream().map(
-                atores -> (AtorDto) new AtorDto().mapearParaDto(atores)
-        ).toList();
-        DiretorDto diretorFilmeDto = (DiretorDto) new DiretorDto().mapearParaDto(filme.getDiretorFilme());
-        return new FilmeDto(
-                filme.getNomeFilme(),
-                filme.getDuracaoTotalMinutos(),
-                filme.getAvaliacaoFilme(),
-                filme.getAnoLancamentoFilme(),
-                filme.getOrcamentoFilme(),
-                filme.getDescricao(),
-                diretorFilmeDto,
-                atoresDto
-        );
+    private List<Field> verificarCamposNulos() throws NoSuchFieldException, IllegalAccessException {
+        List<Field> list = new ArrayList<>();
+        for (Field field : this.getClass()
+                .getDeclaredFields()) {
+            if (verificarValorAtributos(field.getName())) {
+                list.add(field);
+            }
+        }
+        return list;
+    }
+
+    private Boolean verificarValorAtributos(String nomeCampo) throws NoSuchFieldException, IllegalAccessException {
+        Field atributo = this.getClass().getDeclaredField(nomeCampo);
+        return (atributo.get(this) == null)
+                && (!(nomeCampo.contains("descricao"))
+                    && !(nomeCampo.contains("atores"))
+                    && !(nomeCampo.contains("diretor")));
+    }
+
+    public void verificarFilmeValido() {
+        try{
+            if(verificarCamposNulos().isEmpty()) {
+                return;
+            }
+            String camposNulos = formatarMensagemErro();
+            throw new DtoInvalidoException(camposNulos);
+        } catch (NoSuchFieldException | IllegalAccessException exception) {
+            throw new DtoInvalidoException(exception.getMessage());
+        }
+    }
+
+    private String formatarMensagemErro() throws NoSuchFieldException, IllegalAccessException {
+        String camposNulosString;
+        List<String> camposNulos = verificarCamposNulos().stream().map(Field::getName).toList();
+        if (camposNulos.size() > 1) {
+            StringBuilder camposNulosStringBuilder = new StringBuilder();
+            for (String campoNulo : camposNulos) {
+                camposNulosStringBuilder.append(campoNulo).append(", ");
+            }
+            camposNulosStringBuilder.replace(camposNulosStringBuilder.lastIndexOf(","), camposNulosStringBuilder.length(), ".");
+            camposNulosString = String.format("Os campos a seguir não podem ser nulos: %s", camposNulosStringBuilder);
+        } else {
+            camposNulosString = String.format("O campo a seguir não pode ser nulo: %s", verificarCamposNulos().getFirst().getName());
+        }
+        return camposNulosString;
     }
 }
