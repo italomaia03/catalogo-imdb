6517cff4f09de93bfe0815694e2b51dbe0f163d2
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-22 21:59:37 -0300

    :sparkles: feat: tornar público o método verificarAtorEstaCadastrado e tornar cadastrarNovoAtor unicamente responsável por novos cadastros

--- src/main/java/com/adatech/desafiopratico/services/AtorService.java
+++ src/main/java/com/adatech/desafiopratico/services/AtorService.java
@@ -1,8 +1,11 @@
 package com.adatech.desafiopratico.services;
 
 import com.adatech.desafiopratico.dto.AtorDto;
+import com.adatech.desafiopratico.dto.exceptions.DtoInvalidoException;
 import com.adatech.desafiopratico.models.Ator;
 import com.adatech.desafiopratico.repository.AtorRepository;
+import com.adatech.desafiopratico.services.exceptions.CampoInvalidoException;
+import com.adatech.desafiopratico.services.exceptions.NaoEncontradoException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,17 +25,18 @@ public class AtorService {
 
     public Ator cadastrarNovoAtor(AtorDto atorDto){
         try {
-            return verificarAtorEstaCadastrado(atorDto);
-        } catch (NullPointerException e) {
+            atorDto.validarPessoaDto();
             Ator novoAtor = (Ator) atorDto.mapearParaEntidade();
-            return atorRepository.adicionarNovoAtor(novoAtor);
+            return atorRepository.save(novoAtor);
+        } catch (DtoInvalidoException exception) {
+            throw new CampoInvalidoException(exception.getMessage());
         }
     }
 
-    private Ator verificarAtorEstaCadastrado(AtorDto atorDto) {
+    public Ator verificarAtorEstaCadastrado(AtorDto atorDto) {
         Ator ator = atorRepository.findAtorByNomeAtor(atorDto.getNome());
         if (ator == null) {
-            throw new NullPointerException();
+            throw new NaoEncontradoException("Não foi encontrado ator com o nome " + atorDto.getNome() + ".");
         }
         return ator;
     }
