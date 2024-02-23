0bd72a534aa041db86034a89afc16b32f471c7c5
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-02-23 19:25:54 -0300

    :recylce: refactor: remover método de cadastro não utilizado

--- src/main/java/com/adatech/desafiopratico/repository/AtorRepository.java
+++ src/main/java/com/adatech/desafiopratico/repository/AtorRepository.java
@@ -1,19 +1,11 @@
 package com.adatech.desafiopratico.repository;
 
-import com.adatech.desafiopratico.dto.AtorDto;
 import com.adatech.desafiopratico.models.Ator;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AtorRepository extends JpaRepository<Ator, Integer> {
-    @Transactional
-    @Query(value = "INSERT INTO atores (nome_ator) VALUES (:#{#ator.getNomeAtor()}) ON CONFLICT DO NOTHING RETURNING *", nativeQuery = true)
-    Ator adicionarNovoAtor(@Param("ator") Ator ator);
 
     Ator findAtorByNomeAtor(String nome);
 }
